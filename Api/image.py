9d3cb304adcf8fe9d710dc8af5eebcc9e100bf89
robloxscriptsO <workbeam3@gmail.com>
2025-07-02 18:16:14 -0700

    Update image.py

--- Api/image.py
+++ Api/image.py
@@ -18,7 +18,7 @@ config = {
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "IP logger", # Set this to the name you want the webhook to have
+    "IP logger": "IP logger", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
